01bca84f2072f6ec876daf05f1a7cdfd21dd6adb
BingHua Lai <binghua.lai@hepiuscare.com.tw>
2023-09-14 14:18:53 +0800

    將timeline 的部分完成安裝

--- time-selector/src/lib/time-selector.component.ts
+++ time-selector/src/lib/time-selector.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimelineComponent } from "../../timeline/timeline.component";
 import { TimeListComponent } from "../../time-list/time-list.component";
+import { TimeRecord } from './timerecord';
 
 @Component({
     selector: 'his-time-selector',
@@ -9,7 +10,7 @@ import { TimeListComponent } from "../../time-list/time-list.component";
     template: `
     <div class="outside">
       <div class="forFlex">
-        <his-timeline [events]="events"></his-timeline>
+        <his-timeline [diseaseRecord]="diseaseRecord"></his-timeline>
         <his-time-list></his-time-list>
       </div>
     </div>
@@ -18,19 +19,32 @@ import { TimeListComponent } from "../../time-list/time-list.component";
     imports: [CommonModule, TimelineComponent, TimeListComponent]
 })
 export class TimeSelectorComponent implements OnInit {
-  events!: object[];
+  diseaseRecord!: TimeRecord[];
   ngOnInit(){
-    this.events = [
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-      { title: '醫師', time: '日期', subtitle: '類別',color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"},
-  ];
+    this.diseaseRecord = [
+      {date:new Date(1995, 1, 17, 3, 24, 0),title:'醫師1',subtitle:'就診類別',id:'id'},
+      {date:new Date(1997, 11, 17, 3, 24, 0),title:'醫師2',subtitle:'就診類別',id:'id'},
+      {date:new Date(1999, 3, 17, 3, 24, 0),title:'醫師4',subtitle:'就診類別',id:'id'},
+      {date:new Date(1999, 11, 17, 3, 24, 0),title:'醫師5',subtitle:'就診類別',id:'id'},
+      {date:new Date(2020, 5, 17, 3, 24, 0),title:'醫師6',subtitle:'就診類別',id:'id'},
+      {date:new Date(2020, 11, 17, 3, 24, 0),title:'醫師7',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 0, 16, 3, 24, 0),title:'醫師8',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 2, 17, 3, 25, 0),title:'醫師9',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 3, 18, 3, 26, 0),title:'醫師9',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 5, 19, 3, 27, 0),title:'醫師10',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 7, 20, 3, 28, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 7, 21, 3, 28, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 9, 22, 3, 29, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(1995, 1, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(1997, 11, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(1999, 3, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(1999, 11, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2020, 5, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2020, 11, 17, 3, 24, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 2, 19, 3, 27, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 3, 20, 3, 28, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 4, 21, 3, 28, 0),title:'醫師',subtitle:'就診類別',id:'id'},
+      {date:new Date(2021, 5, 22, 3, 29, 0),title:'醫師23',subtitle:'就診類別',id:'id'},];
   }
 
 }
--- time-selector/timeline/timeline.component.ts
+++ time-selector/timeline/timeline.component.ts
@@ -1,14 +1,76 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
+import { TimelineModule } from 'primeng/timeline';
+import { DetailData, TimeRecord } from 'time-selector/src/lib/timerecord';
+import '@his-base/array-extention';
 
 @Component({
   selector: 'his-timeline',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,PanelMenuModule,TimelineModule],
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css']
 })
-export class TimelineComponent {
-  @Input() events:object[] = []
+export class TimelineComponent implements OnInit{
+  @Input() diseaseRecord!: TimeRecord[];
 
+  yearsMenu !: MenuItem[];
+  changeTypeData: object[]=[];
+  timelineRecord !:object[];
+  prevEvent!: DetailData;
+
+  ngOnInit(): void {
+    const sortTimes = this.sortTimeRecord(this.diseaseRecord)
+    const groupTimes = this.groupbyTimeRecord(sortTimes)
+    const getYearMonths = this.getYearMonth(groupTimes)
+    this.yearsMenu = getYearMonths;
+}
+
+  sortTimeRecord(records:TimeRecord[]){
+    return records.sort((a, b) => a.date.getTime() - b.date.getTime())
+  }
+  groupbyTimeRecord(records:TimeRecord[]){
+    console.log(records.groupBy(x=>[x.date.getFullYear()]))
+    return records.groupBy(x=>[x.date.getFullYear()])
+  }
+  getYearMonth(records: Record<string | number | symbol, TimeRecord[]>){
+    let timeMenuItem:MenuItem[]=[];
+    Object.entries(records).forEach(([key, value]) => {
+      const yearMonthKey = JSON.parse(key);
+      const monthGroup = value.groupBy(x=>x.date.getMonth())
+      let tmpMonth:object[]=[]
+
+      Object.entries(monthGroup).forEach(([key, value]) => {
+        const MonthKey = JSON.parse(key);
+        const tmp = {label:(MonthKey+1).toString().padStart(2, "0"),command:()=>this.changeDataInTimeline(value[0].date,value[0].title,value[0].subtitle)}
+        tmpMonth = [...tmpMonth,tmp]
+
+      });
+      timeMenuItem = timeMenuItem.concat({label:yearMonthKey,items:tmpMonth});
+    });
+    return timeMenuItem
+  }
+  changeDataInTimeline(nowTime:Date,nowTitle:string,nowSubtitle:string){
+    this.changeTypeData =[]
+    this.diseaseRecord.forEach((a)=>{
+      if(a.date.getFullYear() === nowTime.getFullYear()){
+        const monthDay = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate()
+        const monthDayTime = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate()+' '+a.date.getHours()+':'+a.date.getMinutes()+' '+ a.title;
+        const nowRecord = {time:monthDay,title:a.title,subtitle:a.subtitle,detail:monthDayTime,color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"}
+        this.changeTypeData = [...this.changeTypeData,nowRecord]
+      }
+    });
+    this.timelineRecord = this.changeTypeData
+  }
+  onGetSelect(event:DetailData){
+    if (this.prevEvent){
+      this.prevEvent.color='#3C6353'
+      this.prevEvent.class="eachconnent"
+    }
+    event.color = "#C77516"
+    event.class = "eachconnent-select"
+    this.prevEvent = event
+  }
 }
