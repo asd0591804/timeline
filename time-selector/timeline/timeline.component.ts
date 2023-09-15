56e5c7702cb0f2fd6b35a09364e41ea0d572e03c
BingHua Lai <binghua.lai@hepiuscare.com.tw>
2023-09-15 14:11:27 +0800

    可以捲動同步選單

--- time-selector/timeline/timeline.component.ts
+++ time-selector/timeline/timeline.component.ts
@@ -30,15 +30,14 @@ export class TimelineComponent implements OnInit{
   ngOnInit(): void {
     const sortTimes = this.sortTimeRecord(this.diseaseRecord);
     const groupTimes = this.groupbyTimeRecord(sortTimes);
-    const getYearMonths = this.getYearMonth(groupTimes);
-    this.yearsMenu = getYearMonths;
+    this.yearsMenu = this.getYearMonth(groupTimes);
+
   }
 
   sortTimeRecord(records:TimeRecord[]){
     return records.sort((a, b) => a.date.getTime() - b.date.getTime());
   }
   groupbyTimeRecord(records:TimeRecord[]){
-    console.log(records.groupBy(x=>[x.date.getFullYear()]));
     return records.groupBy(x=>[x.date.getFullYear()]);
   }
   getYearMonth(records: Record<string | number | symbol, TimeRecord[]>){
@@ -50,29 +49,26 @@ export class TimelineComponent implements OnInit{
 
       Object.entries(monthGroup).forEach(([key, value]) => {
         const MonthKey = JSON.parse(key);
-        const tmp = {label:(MonthKey+1).toString().padStart(2, "0"),command:()=>this.changeDataInTimeline(value[0].date,value[0].title,value[0].subtitle)};
+        const tmp = {label:(MonthKey+1).toString().padStart(2, "0"),command:()=>this.changeDataInTimeline(value[0].date)};
         tmpMonth = [...tmpMonth,tmp];
-
       });
       timeMenuItem = timeMenuItem.concat({label:yearMonthKey,items:tmpMonth});
     });
     return timeMenuItem;
   }
-  changeDataInTimeline(nowTime:Date,nowTitle:string,nowSubtitle:string){
+  changeDataInTimeline(nowTime:Date){
     this.changeTypeRecord =[];
     this.diseaseRecord.forEach((a)=>{
-    const monthDay = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate();
-    const monthDayTime = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate()+' '+a.date.getHours()+':'+a.date.getMinutes()+' '+ a.title;
-    const nowRecord = {time:monthDay,title:a.title,subtitle:a.subtitle,detail:monthDayTime,color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"};
-    this.changeTypeRecord = [...this.changeTypeRecord,nowRecord];
+      const monthDay = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate();
+      const monthDayTime = (a.date.getMonth()+1).toString().padStart(2, "0")+'-'+a.date.getDate()+' '+a.date.getHours()+':'+a.date.getMinutes()+' '+ a.title;
+      const nowRecord = {time:monthDay,title:a.title,subtitle:a.subtitle,detail:monthDayTime,color: '#3C6353',style:"border: 0px solid #3C6353;border-radius: 50%;width: 1rem;height: 1rem;"};
+      this.changeTypeRecord = [...this.changeTypeRecord,nowRecord];
     });
     this.timelineRecord = this.changeTypeRecord;
 
-    const selectedIndex = this.diseaseRecord.findIndex(x=>x.date === nowTime);
-    const recordLength = this.diseaseRecord.length;
-    this.scrollToTarget(selectedIndex,recordLength);
+    this.scrollToTarget(nowTime);
   }
-  onGetSelect(selectedTime:DetailData){
+  onSelectTime(selectedTime:DetailData){
     if (this.previousSelect){
       this.previousSelect.color='#3C6353';
       this.previousSelect.class="eachconnent";
@@ -82,15 +78,16 @@ export class TimelineComponent implements OnInit{
     this.previousSelect = selectedTime;
   }
 
-  scrollToTarget(height:number,total:number) {
+  scrollToTarget(nowTime:Date) {
     /**為了等時間軸生成用promise，等到好後可捲動畫面，沒有的話按第一次不會捲動 */
     return new Promise(() => {
       setTimeout(() => {
+        const selectedIndex = this.diseaseRecord.findIndex(x=>x.date === nowTime);
+        const recordLength = this.diseaseRecord.length;
         const scrollBody = document.getElementById('scrollTable');
         const timeline = document.getElementById('timeline');
         if (timeline){
-          const percent = height / total;
-          console.log(timeline.scrollTop);
+          const percent = selectedIndex / recordLength;
           const target = (percent)*(timeline.offsetHeight)-10;
           scrollBody?.scrollTo(0,target);
         }
@@ -98,14 +95,11 @@ export class TimelineComponent implements OnInit{
     });
   }
 
+  /**測試按鈕用的function 可刪 */
   onClickActive(){
-    // const yearPanel = document.getElementById('yearsPanel');
-    // yearPanel.active()
-    console.log('----');
     const x = '2021'
     const searchLabel = '[aria-label="'+x+'"]'
     const yearsPanel = document.querySelector(searchLabel);
-    console.log(yearsPanel);
     const clickEvent = new MouseEvent('click', {
       bubbles: true,
       cancelable: true,
@@ -122,32 +116,43 @@ export class TimelineComponent implements OnInit{
     this.inTimeline = false;
   }
 
-  @HostListener('scroll', ['$event']) onElementScroll($event: any) {
-    // console.log($event.offsetTop); //有監聽到事件
+  @HostListener('scroll', ['$event']) onElementScroll() {
+    //監聽到捲軸滾動事件
     const modal= this.el.nativeElement.querySelector('#scrollTable');
-    console.log(modal.scrollTop);
+    const timeline = document.getElementById('timeline');
+    if(timeline){
+      //找到目前的高度比例
+      const percent = ((modal.scrollTop)/timeline.offsetHeight);
+      //找出對應的目標在哪一年
+      const searchLabel = this.findTargetYears(percent);
+      this.openTimeMenu(searchLabel);
+    }
+  }
 
-    const x = '1997'
-    const searchLabel = '[aria-label="'+x+'"]'
-    const yearsPanel = document.querySelector(searchLabel);
-    console.log(yearsPanel);
+  findTargetYears(percent:number){
+     //依比例去找出目標在哪一年
+     const targetIndex = Math.floor(percent*this.diseaseRecord.length);
+     const years = this.diseaseRecord[targetIndex+1].date.getFullYear();
+     //找到對應的menu選單label
+     const searchLabel = '[aria-label="'+years+'"]'
+     return searchLabel;
+  }
+  openTimeMenu(searchLabel:string){
     const clickEvent = new MouseEvent('click', {
       bubbles: true,
       cancelable: true,
       view: window,
     });
+    const yearsPanel = document.querySelector(searchLabel);
+    //如果選單存在在執行
     if(yearsPanel){
+      //確認選單是否有打開
       const ariaExpanded = yearsPanel.getAttribute('aria-expanded');
-      const isSelected = ariaExpanded === "true";
-      if(!isSelected && this.inTimeline){
+      const expandedState = ariaExpanded === "true";
+      //如果不是打開狀態，以及確認了滑鼠在timeline之中(代表使用者在捲動)，再點開，
+      if(!expandedState && this.inTimeline){
         yearsPanel.dispatchEvent(clickEvent);
       }
     }
-
-  }
-
-   reverseBoolean(value: boolean) {
-    console.log('in function value :' , value);
-    return !value;
   }
 }
